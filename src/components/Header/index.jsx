c1b5e96135dc302f28c2bc845cbfa39c0ed0fe1b
Vitor <vitorfonsecaxs@gmail.com>
2025-09-29 11:07:41 -0300

    MODIFY: Estilização do header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -8,7 +8,7 @@ export default function Header({ current, total, time }) {
             <h1>Quiz Interativo</h1>
             <div className={styles.info}>
                 <span>Questão {current} / {total}</span>
-                <span>Timer: {time}s</span>
+                <span>Tempo ⏳: {time}s</span>
             </div>
         </header>
     );
